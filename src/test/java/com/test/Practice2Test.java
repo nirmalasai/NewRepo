ce0d79da67e72822a3be844d11f49e3f79a5fd8f
Nirmala <sakhinetipalli.nirmala@gmail.com>
2022-07-25 12:59:45 +0530

    commit1

---
+++ src/test/java/com/test/Practice2Test.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+	public class Practice2Test 
+	{
+		 @Test
+			public void test2() 
+		 {
+			 Reporter.log("test2",true);
+				//hello
+				
+				//change
+		 } 
+				 }
+
---
+++ src/test/java/com/test/Prctice3Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Prctice3Test 
+{
+	 @Test
+
+		public void test3() 
+	 {
+		 Reporter.log("all is well",true);
+		
+	 } 
+}
+
+
